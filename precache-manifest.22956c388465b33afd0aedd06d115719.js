289d4156375fbcbf57ac40f03e7e157c7facbce8
kumarkeshri2185 <kumarkeshri2185@gmail.com>
2025-01-02 23:13:59 +0530

    Updates

---
+++ precache-manifest.22956c388465b33afd0aedd06d115719.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44373188bb70f672e6b2ce0f90f0047d",
+    "url": "/SmartGen/index.html"
+  },
+  {
+    "revision": "65aa54987867e01cb0f1",
+    "url": "/SmartGen/static/css/main.a9bca2ef.chunk.css"
+  },
+  {
+    "revision": "6552fa4abe12de437515",
+    "url": "/SmartGen/static/js/2.ca7447be.chunk.js"
+  },
+  {
+    "revision": "20f46a6d5c56b6bc85c4a5647ee397c4",
+    "url": "/SmartGen/static/js/2.ca7447be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65aa54987867e01cb0f1",
+    "url": "/SmartGen/static/js/main.c613b274.chunk.js"
+  },
+  {
+    "revision": "582765d5c7412d2ca232",
+    "url": "/SmartGen/static/js/runtime-main.87850482.js"
+  },
+  {
+    "revision": "f76dc7d6a05aa59a19db988927cab9d6",
+    "url": "/SmartGen/static/media/Logo.f76dc7d6.jpeg"
+  },
+  {
+    "revision": "1baa13aa7584a12d40f2940d0623cfa5",
+    "url": "/SmartGen/static/media/background.1baa13aa.jpg"
+  }
+]);
